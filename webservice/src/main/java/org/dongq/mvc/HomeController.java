78c3d5509869e69a2a0f7560bc82b47e2778f562
EastSeven <eastseven@foxmail.com>
2012-09-18 22:42:37 +0800

    move bootstrap to resources folder

--- webservice/src/main/java/org/dongq/mvc/HomeController.java
+++ webservice/src/main/java/org/dongq/mvc/HomeController.java
@@ -20,8 +20,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HomeController {
 
-	private static final Logger logger = LoggerFactory
-			.getLogger(HomeController.class);
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 
 	/**
 	 * Simply selects the home view to render by returning its name.
@@ -31,8 +30,7 @@ public class HomeController {
 		logger.info("Welcome home! the client locale is " + locale.toString());
 
 		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG,
-				DateFormat.LONG, locale);
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 
 		String formattedDate = dateFormat.format(date);
 
